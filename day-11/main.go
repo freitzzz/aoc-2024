f0178695d83c706efa57f3e7836d20b62db44213
João Freitas <1160907@isep.ipp.pt>
2024-12-11 21:53:41 +0000

    day 11

--- day-11/main.go
+++ day-11/main.go
@@ -16,6 +16,8 @@ var input string
 //go:embed input-test.txt
 var input_test string
 
+var cacheFn = map[[2]int]int{}
+
 func init() {
 	flag.BoolFunc("test", "uses input-test.txt", func(s string) error {
 		input = input_test
@@ -35,6 +37,7 @@ func main() {
 	}
 
 	println(part1(stones, 25))
+	println(part2(stones, 75))
 }
 
 func part1(stones []int, count int) int {
@@ -63,6 +66,47 @@ func part1(stones []int, count int) int {
 	}
 
 	return len(stones)
+}
+
+func part2(stones []int, count int) int {
+	sum := 0
+	for _, st := range stones {
+		sum += part2Memoization(st, count)
+	}
+
+	return sum
+}
+
+func part2Memoization(stone int, count int) int {
+	if count == 0 {
+		return 1
+	}
+
+	if m, ok := cacheFn[[2]int{stone, count}]; ok {
+		return m
+	}
+
+	if stone == 0 {
+		return memoizePart2Memoization(1, count-1)
+	}
+
+	if s := strconv.Itoa(stone); len(s)%2 == 0 {
+		split := []int{
+			mustInt(s[:len(s)/2]),
+			mustInt(s[len(s)/2:]),
+		}
+
+		return memoizePart2Memoization(split[0], count-1) + memoizePart2Memoization(split[1], count-1)
+	}
+
+	r := stone * 2024
+	return memoizePart2Memoization(r, count-1)
+}
+
+func memoizePart2Memoization(stone, count int) int {
+	result := part2Memoization(stone, count)
+	cacheFn[[2]int{stone, count}] = result
+	return result
 }
 
 func mustInt(s string) int {
